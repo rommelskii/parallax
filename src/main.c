5eb759dbcbf4e0bc345b2c4562babdc5bb5a1eee
mels <rommel.ronduen2244@gmail.com>
2025-10-03 02:29:18 +0800

    (KAN-6) modified argument guard
    uses variable for number of arguments + more verbose error

--- src/main.c
+++ src/main.c
@@ -3,10 +3,14 @@
 
 int main(int argc, char argv[])
 {
-  if (argc == 1) 
+  const int NUM_OF_ARGS = argc - 1;
+  if (NUM_OF_ARGS < 2) 
   {
+    printf("Error: Expected 2 or more arguments, got %d\n", NUM_OF_ARGS);
     printf("Usage: prlx [FLAGS] [CONTENT/IDENTIFIER] [CONTENT]\n");
+    return -1;
   }
+
 
   return 0;
 }
